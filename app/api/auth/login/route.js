3199ed12e8014b5120f051cc19d9124f3c22f966
abu sayed <abusayed.in22@gmail.com>
2025-02-10 21:52:56 +0600

    okay

--- app/api/auth/login/route.js
+++ app/api/auth/login/route.js
@@ -1,9 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-import bcrypt from 'bcryptjs'; // For password hashing verification
+import bcrypt from 'bcrypt'; // For password hashing verification
 import { SignJWT } from "jose";
 import { CreateToken } from "@/lib/tokenHelper";
 import { cookies } from "next/headers";
+import { hashPassword } from "@/lib/passwordHandler";
 
 
 const prisma = new PrismaClient();
@@ -17,8 +18,9 @@ export const POST = async (req) => {
         const { email, password } = await req.json();
 
         const existingUser = await prisma.admin.findUnique({
-            where: { email:email,password:password },
+            where: { email:email },
         });
+        
 
         if (!existingUser) {
             return NextResponse.json({ error: 'User does not exist.' }, { status: 404 });
@@ -27,16 +29,29 @@ export const POST = async (req) => {
 
         // create token
         const token = await CreateToken(existingUser['email'], existingUser['id']);
-        // expiretion time 
-        const expirationTime = new Date(Date.now() + 60 * 60 * 1000)
+        const expirationTime = new Date(Date.now() + 60 * 60 * 1000) // expiretion time 
         const cookieString = `token=${token};expires=${expirationTime.toUTCString()};path=/`;
 
+        // if("$2a$10$uVlir4YLj47uLt4kWKG9vOPNMnpP0bfQUCCHQRr6NulrdA5oMySr6" === "$2a$10$uVlir4YLj47uLt4kWKG9vOPNMnpP0bfQUCCHQRr6NulrdA5oMySr6"){
+        //     console.log('correct')
+        // }
+
+        // match password
+        // Compare the entered password with the hashed password
+        const isMatch = await bcrypt.compare(password, existingUser.password);
+        console.log(isMatch)
+        if(!isMatch){
+            return NextResponse.json({ error: 'Password incorrect'});
+        }
+
+
+
         // update token on database 
-        await prisma.admin.update({
+        const dataCreate = await prisma.admin.update({
             where: {
                 id: parseInt(existingUser.id),
                 email: existingUser.email,
-                password: password
+                // password: password
             },
             data: {
                 token: token,
@@ -47,7 +62,7 @@ export const POST = async (req) => {
         // Set token in an HTTP-only cookie
         return NextResponse.json({ status: "Login successful", data: token });
     } catch (error) {
-        console.error(error);
+        // console.log(error);
         return NextResponse.json({ error: 'An error occurred.' }, { status: 500 });
     }
 };
--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,3 +1,5 @@
+import { hashPassword } from "@/lib/passwordHandler";
+import { EncodedPassword } from "@/lib/tokenHelper";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
@@ -10,7 +12,6 @@ export const POST = async (req,res) => {
     try {
         let dataObj = await req.json();
         dataObj['otp'] = 0;
-        console.log(dataObj)
 
         // check user exist
         const existingUser = await prisma.admin.findUnique({
@@ -18,12 +19,20 @@ export const POST = async (req,res) => {
                 email: dataObj.email,
             }
         });
+        // console.log(existingUser)
 
         if (existingUser) {
-            return NextResponse.json({ Error: 'User already exists, please login' }, { status: 400 });
+            // console.log('User already exists, please login!')
+            return NextResponse.json({ Error: 'User already exists, please login!' }, { status: 400 });
         }
 
+        // password encoded
+        const hashedPassword = await hashPassword(dataObj.password);
+        console.log(hashedPassword);
 
+        if(!hashedPassword) {
+            return NextResponse.json({ Error: 'Password Hashed error' }, { status: 400 });
+        }
 
          
         // Create a new user in the database
@@ -31,10 +40,15 @@ export const POST = async (req,res) => {
             data: {
                 fullname: dataObj.fullName,
                 email: dataObj.email,
-                password: dataObj.password, 
+                password: hashedPassword, 
                 otp: '0',
                 token: ''
             },
+            select:{
+                fullname:true,
+                email:true,
+                password:true
+            }
         });
                 
 
