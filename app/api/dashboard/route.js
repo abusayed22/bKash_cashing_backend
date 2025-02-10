bff0ed8e70f41314dbb00bbbd0d2d83ef9f2dc96
abu sayed <abusayed.in22@gmail.com>
2025-02-10 12:03:46 +0600

    ok

--- app/api/dashboard/route.js
+++ app/api/dashboard/route.js
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 import { ResponseMes, verificationAuthor } from "@/lib/Globalfunction";
-// import prisma from "@/lib/Prisma";
+import prisma from "@/lib/Prisma";
 import { NextResponse } from "next/server";
 
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
 
 
